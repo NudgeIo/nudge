d0116bc208280366f9f7d9ee8d5b1a73ebcf8b5c
edisonqu <edisonquu@gmail.com>
2023-08-24 23:37:51 -0700

    feat: updated scopes to the authentication

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth, { User, Account, Profile } from 'next-auth';
 import GoogleProvider from "next-auth/providers/google"
 import { useRouter } from 'next/router';
-import prisma from '../lib/prisma';
+import prisma from '../../../lib/prisma';
 import { Provider } from '@prisma/client';
 
 
@@ -11,20 +11,34 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          scope: 'openid email profile https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube.force-ssl',
+        },
+      }
     }),
 
   ],
     callbacks: {
       async signIn(params) {
-        const { user, account, profile } = params
+        const { user, account, profile, credentials} = params
         if(!user) return false;
+        if(!account) return false;
+  
 
         let nudgeUser = await prisma.nudgeUser.findUnique({
           where: {email: user.email as string}
         })
-
-        console.log("User exists");
+        const accessToken = account.access_token
+        console.log("This is the access token", accessToken);
         
+        
+        const response = await fetch("https://www.googleapis.com/youtube/v3/channels?part=snippet&mine=true", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        })
+
 
         if(nudgeUser) return true;
 
@@ -50,7 +64,10 @@ export default NextAuth({
 
         const { session, token: jwtToken } = params;
 
-        (session.user as any).token = jwtToken.jwt;
+        // (session.user as any).token = jwtToken.jwt;
+        session.user = jwtToken
+
+        // session.accessToken = jwtToken.accessToken;
 
         console.log("Session", session);
         console.log("UserOrToken", jwtToken);
@@ -79,6 +96,9 @@ export default NextAuth({
             avatarUrl: nudgeUser.avatarUrl,
             provider: nudgeUser.provider,
           }
+        }
+        if (account) {
+          token.accessToken = account.access_token
         }
         return token;
       }, 

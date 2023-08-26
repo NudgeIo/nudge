fb42087dd40ee3b6a9321a3c7d86fb1cb8677eab
edisonqu <edisonquu@gmail.com>
2023-08-25 23:54:04 -0700

    fix: image problems

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+
+  
+// }
+
+module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['yt3.googleusercontent.com','yt3.ggpht.com','i.ytimg.com'],
   },
-  
-}
-
-module.exports = {
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  },
-  nextConfig
+  }
 };

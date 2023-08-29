de1c5845a78a278fea2ce561e0283b3934b6dab2
Yves Shum <yvesshum1210@gmail.com>
2023-08-28 23:06:48 -0700

    update eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  ignorePatterns: [
+    'prisma/**/*.ts',
+    'public/**/*.js',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
